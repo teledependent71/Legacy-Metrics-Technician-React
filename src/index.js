5783badd2a31cfb1694707521872cd9e167f9c5d
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2023-09-28 18:30:34 +0300

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './style.css'
 import Home from './views/home'
+import NotFound from './views/not-found'
 
 const App = () => {
   return (
     <Router>
       <div>
         <Route component={Home} exact path="/" />
+        <Route component={NotFound} path="**" />
       </div>
     </Router>
   )
